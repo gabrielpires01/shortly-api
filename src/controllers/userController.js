9fae103ed221d9d3ef88f3b71211b6b831365248
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-17 22:15:32 -0300

    Get One User Route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -35,3 +35,28 @@ export async function getUser(req, res) {
     return res.sendStatus(500);
   }
 }
+
+export const getOneUser = async(req,res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await connection.query(`
+      SELECT users.id, users.name, SUM(urls.visits) AS "visitCount"
+      FROM urls 
+        JOIN users ON urls.user_id = users.id
+      WHERE users.id = $1
+      GROUP BY users.id`, [id])
+    
+    if(!user.rowCount) return res.sendStatus(404)
+
+    const shortnedUrls = await connection.query(`
+      SELECT u.id, u.short_url AS "shortUrl", u.url, u.visits AS "visitCount"
+      FROM urls u
+      WHERE u.user_id = $1`, [id]);
+
+    const response = {...user.rows[0], shortnedUrls: shortnedUrls.rows}
+    return res.status(200).send(response)
+  }catch(err){
+    return res.status(500).send(err)
+  }
+}
